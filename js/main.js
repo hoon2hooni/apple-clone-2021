fe07ec25575e6a0d0e7cc7889257805233e26b3e
Sanghoon Han <sanghoon.engineer@gmail.com>
2021-01-12 21:01:14 +0900

    add scrollbar option with javascript

--- js/main.js
+++ js/main.js
@@ -1,49 +1,62 @@
 
 (()=>{
     const sceneInfo = [
-        { 
+        {   
+            //o
             type: "sticky",
             heightNum: 5, //브라우저 높이의 5배로 sccrollHeight 세팅  디바이스 마다 다르기에 이렇게 해줌
             scrollHeight: 0, // 스크롤높이 스크롤의 몇배수
             objs: {
                 container: document.querySelector('#scroll-section-0')
-            },
+            }
         },
-        {
+        {   
+            //1
             type:"normal",
             heightNum: 5, 
             scrollHeight: 0,
             objs: {
                 container: document.querySelector('#scroll-section-1')
-            },
+            }
         },
-        {
+        {   
+            //2
             type:"sticky",
             heightNum: 5, 
             scrollHeight: 0, 
             objs: {
                 container: document.querySelector('#scroll-section-2')
-            },
+            }
         },
-        {
+        {   
+            //3
             type:"sticky",
             heightNum: 5, 
             scrollHeight: 0,
             objs: {
                 container: document.querySelector('#scroll-section-3')
-            },
+            }
         }
         
     ];
-    function setLayout(){
+    
+    function setLayout( ){
         // 각 스크롤 섹션의 높이 세팅
-        for (let i =0; i < sceneInfo.length; i++){
+        for  (let i =0; i < sceneInfo.length; i++){
             sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
             sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
         }
-
+    }
+    function scrollLoop( ){
+        console.log(window.pageYOffset);
     }
 
-})
+    window.addEventListener("resize", setLayout());
+    window.addEventListener("scroll", () => {
+        scrollLoop( );
+    });
+
+    setLayout();
+}) ( );
 // (function() {})();
 // 전역변수 말고 지역변수 사용하기 위해 함수에 넣어줌
