839ccfab6ae5f1356e0e8a9dd298d5bbfe5aa9ae
Sanghoon Han <sanghoon.engineer@gmail.com>
2021-01-15 17:20:00 +0900

    add part interacting to current scroll locaiton which show body or not

--- js/main.js
+++ js/main.js
@@ -49,6 +49,16 @@
             sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
             sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
         }
+
+        let yOffset = window.pageYOffset;
+        let totalScrollHeight  = 0; //현재 창 새로고침 했을때 current scene 지정
+        for (let i = 0; i  < sceneInfo.length; i++) { // 현재 스크롤 위치랑 totalscrollheigh 비교
+            totalScrollHeight += sceneInfo[i].scrollHeight;
+            if (totalScrollHeight >= yOffset){ // 만약 total scrollHeight 가 현재 스크롤 위치보다 커지면 break 하고 i값을 current로 넣어줌
+                currentScene = i;
+                break; 
+            }
+        }
     }
     
     function scrollLoop( ){
@@ -58,11 +68,15 @@
         }
         if  (yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight){
             currentScene ++ ;
+            document.body.setAttribute("id", `show-scene-${currentScene}`);
+            //나중에 다시 수정해야 할 부분 
         }
         if (yOffset < prevScrollHeight){
             if (currentScene ===0) return;
             currentScene --;
+            document.body.setAttribute("id", `show-scene-${currentScene}`);
         }
+        // document.body.setAttribute("id", `show-scene-${currentScene}`);
         //  내려갈때 yOffset  >  prevScrollHeight + section -> currentScent  += 1 
         //  올라갈때  yOffset  < prevScrollHeight - > currentScene  -= 1
         // 메뉴떄매  딱맞아 덜어지지 않음
@@ -76,8 +90,11 @@
         // console.log(yOffset)
         scrollLoop( );
     });
-
-    setLayout();
+    // window.addEventListener("DOMContentLoaded", setLayout);
+    window.addEventListener("load", setLayout);
+    window.addEventListener("resize", setLayout);
+    // setLayout();
+    // 문서가 다 완전히  로드되면 
 }) ( );
 // (function() {})();
 // 전역변수 말고 지역변수 사용하기 위해 함수에 넣어줌
